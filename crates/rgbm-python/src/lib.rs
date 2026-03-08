379d1e900e4132b689139097ba9b87f7084ae405
Malte Londschien <malte@londschien.de>
2026-03-08 12:19:56 -0400

    Initial commit.

---
+++ crates/rgbm-python/src/lib.rs
@@ -0,0 +1,6 @@
+use pyo3::prelude::*;
+
+#[pymodule]
+fn rgbm(_m: &Bound<'_, PyModule>) -> PyResult<()> {
+    Ok(())
+}
---
+++ crates/rgbm/src/lib.rs
@@ -0,0 +1,6 @@
+pub mod bin;
+pub mod booster;
+pub mod dataset;
+pub mod histogram;
+pub mod objective;
+pub mod tree;
